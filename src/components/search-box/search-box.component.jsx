a4723f8720631821c1d50c6c8716973ee298d373
Tiago Viegas <tiagomviegas88@gmail.com>
2022-10-03 16:18:42 +0100

    Main Project

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,12 @@
+import './search-box.styles.css'
+
+const SearchBox = ({className, onChangeHandler, placeholder})=> (
+    <input 
+    className={`search-box ${className}`}
+    type='search' 
+    placeholder={placeholder}
+    onChange={onChangeHandler}
+    />
+    )
+
+export default SearchBox;
